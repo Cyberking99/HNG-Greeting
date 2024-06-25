82d02c8631e4948512a95ca3164f0ab5ad842045
Kefas Kingsley <kefasiceking@gmail.com>
2024-06-25 18:56:53 +0100

    update : Used request-ip package to get client IP

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const request_ip = require('request-ip')
 const app = express();
+app.set('trust proxy', true)
 
 app.get('/api/hello', (req, res) => {
   const visitor_name = (req.query.visitor_name != undefined?req.query.visitor_name:"visitor");
-  const client_ip = req.ip;
+  const client_ip = request_ip.getClientIp(req)
   // const location = ''; // todo: implement location lookup
   const greeting = `Hello, ${visitor_name}!`;
 
