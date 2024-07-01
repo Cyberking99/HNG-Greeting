5077b3fc7c0127c1c23b99c1d56f8f84d441bcdf
Kefas Kingsley <kefasiceking@gmail.com>
2024-07-01 21:09:26 +0100

    feature: added location to response and temperature to greeting

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const request_ip = require('request-ip')
 const app = express();
 app.set('trust proxy', true)
 
 app.get('/api/hello', (req, res) => {
   const visitor_name = (req.query.visitor_name != undefined?req.query.visitor_name:"visitor");
-  // const client_ip = request_ip.getClientIp(req)
   const client_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  // const location = ''; // todo: implement location lookup
-  const greeting = `Hello, ${visitor_name}!`;
-
-  res.json({
-    client_ip: client_ip,
-    // location,
-    greeting
-  });
+  axios.get('https://api.weatherapi.com/v1/current.json?q=146.70.99.187&key=b14c92e2cc6549da997194413240107')
+    .then(response => {
+      const location = response.location.name;
+      const temperature = response.current.temp_c;
+      const greeting = `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celcius in ${location}`;
+      res.json({
+        client_ip,
+        location,
+        greeting
+      });
+    });
 });
 
 app.listen(3000, () => {
