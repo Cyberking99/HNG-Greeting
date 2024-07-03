7b33201218f678fc73ab5e817b7c17149deab57b
Kefas Kingsley <kefasiceking@gmail.com>
2024-07-03 21:22:25 +0100

    update: remove special characters around name and extract specific data from response object of the weather api

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const axios = require('axios');
 app.set('trust proxy', true)
 
 app.get('/api/hello', (req, res) => {
-  const visitor_name = (req.query.visitor_name != undefined?req.query.visitor_name:"visitor");
-  const client_ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const visitor_name = (req.query.visitor_name != undefined?req.query.visitor_name.replace(/["]/g, ""):"visitor");
+  const client_ip = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0].trim() : req.socket.remoteAddress;
   axios.get('https://api.weatherapi.com/v1/current.json?q=146.70.99.187&key=b14c92e2cc6549da997194413240107')
     .then(response => {
-      const location = response.location.name;
-      const temperature = response.current.temp_c;
+      const data = response.data;
+      const location = data.location.name;
+      const temperature = data.current.temp_c;
       const greeting = `Hello, ${visitor_name}!, the temperature is ${temperature} degrees Celcius in ${location}`;
       res.json({
         client_ip,
